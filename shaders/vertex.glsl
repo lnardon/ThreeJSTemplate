cb1ed615244fa568b2a2581280cf951b206240fe
lnardon <lucaslnardon@gmail.com>
2021-04-09 02:11:44 -0300

    ✨ Add shaders

---
+++ shaders/vertex.glsl
@@ -0,0 +1,7 @@
+void main(){
+    vUv = uv;
+
+    vec4 mvPosition = modelViewMatrix * vec4(position,1.0);
+    gl_PointSize = size*5. * (1. / -mvPosition.z);
+    gl_Position = projectionMatrix * mvPosition;
+}
